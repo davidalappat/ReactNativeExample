6950fa09a49da204a2f45be1f451b93f0c594ddb
davidalappat <davidalappat@gmail.com>
2022-04-17 23:03:42 +0530

    Feature 1 ready!

--- App.js
+++ App.js
@@ -1,21 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import Greeting from "./components/greeting";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <TextInput value="David Alappat" />
-      <Text>Welcome to this CI/CD course!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Greeting />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ components/greeting.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+const Greeting = () => {
+  const [name, setName] = useState("");
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={name}
+        place="Write your name.."
+        onChangeText={(name) => setName(name)}
+      />
+      <Text>Welcome to this CI/CD course, {name}!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: { height: 30, borderColor: "gray", borderWidth: 1, width: 200 },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Greeting;
